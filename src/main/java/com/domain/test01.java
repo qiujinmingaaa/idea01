96f7d3e245f1284d2bfce705b2ddc61df7eeb7e8
qjm <695590221@qq.com>
2019-08-28 20:47:01 +0800

    增加了123

--- src/main/java/com/domain/test01.java
+++ src/main/java/com/domain/test01.java
@@ -11,4 +11,5 @@ public class test01 {
    public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("2222");
-    }}
+    }
+}
--- src/main/java/com/domain/test03.java
+++ src/main/java/com/domain/test03.java
@@ -9,5 +9,6 @@ package com.domain;
 public class test03 {
     public static void main(String[] args) {
         System.out.println("====");
+        System.out.println("123");
     }
 }
